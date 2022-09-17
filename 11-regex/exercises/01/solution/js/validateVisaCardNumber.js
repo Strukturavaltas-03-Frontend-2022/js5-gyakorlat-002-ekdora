462640599c951db7b745e67754c7a87e9f9ff5f8
ekdora <dora.egrykiss@gmail.com>
2022-09-17 19:33:30 +0200

    Tasks solution

--- 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
+++ 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
@@ -1,1 +1,7 @@
+const visaRegexp = /^[4]\d{15}$/;
+
+const validateVisaCardNumber = (visaNumber) => {
+    return visaRegexp.test(visaNumber);
+}
+
 export default validateVisaCardNumber;
--- 11-regex/exercises/02/solution/js/validateIPAddress.js
+++ 11-regex/exercises/02/solution/js/validateIPAddress.js
@@ -1,1 +1,7 @@
+const IPv4Regexp = /(^(0\.0\.0\.0)$|^(((([1-9]|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5]))\.)(((\d|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5]))\.){2}((\d|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5])))$)/;
+
+const validateIPAddress = (IPv4Number) => {
+    return IPv4Regexp.test(IPv4Number);
+}
+
 export default validateIPAddress;
